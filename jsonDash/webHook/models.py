417538450ed483a1561454aa76504aaa013a7d72
Vinogradov Anton <vino74974@gmail.com>
2024-10-02 12:40:42 +0300

    feat: postgreSQL connection

---
+++ jsonDash/webHook/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class jsonNode(models.Model):
+    key = models.CharField()
+    value = models.TextField()
+    parent_id = models.IntegerField(null=True,default=True)
+
+    def __str__(self):
+        return self.key +" " + self.value
---
+++ jsonDash/webHook/views.py
@@ -0,0 +1,26 @@
+from .models import jsonNode
+from django.forms import ValidationError
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status
+import logging
+from .models import jsonNode
+from .serializers import Node_Serializer
+# Create your views here.
+
+class Node_API_View(viewsets.ModelViewSet):
+
+    queryset = jsonNode.objects.all()
+    serializer_class = Node_Serializer
+
+    def retrive(self,request,*args,**Kwarfs):
+        logging.info(f"{request} - list all objects")
+        data = jsonNode.objects.raw('''
+        WITH RECURSIVE jsonNode(id,parent_id) AS (
+                SELECT id, parent_id
+                FROM jsonNode
+                WHERE id = kwarfs["id"]
+            UNION ALL
+            )
+        SELECT * FROM jsonNode''')                          
+        return Response(data,status=status.HTTP_200_OK)
